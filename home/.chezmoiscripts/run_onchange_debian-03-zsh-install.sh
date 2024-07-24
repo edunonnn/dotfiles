8c455ba87c737a97360a7d285b4ca51920a535e6
Sherman Hsu <edunonnn@gmail.com>
2024-07-24 15:49:03 +0800

    fix: fail to apply scripts on root user in docker

--- home/.chezmoiscripts/run_onchange_debian-03-zsh-install.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-sudo apt install -yy zsh
-
-[ "$(basename ${SHELL})" != "zsh" ] && sudo chsh -s $(which zsh) $(id -un)
-
-# oh-myzsh handle in .chezmoiexternal.toml.tmpl
-# [ ! -d ~/.oh-my-zsh ] && git clone https://github.com/ohmyzsh/ohmyzsh ~/.oh-my-zsh
